2f67701f3e31ab9e1d52c4fb2e71b58cd792e123
minhphanhvu <minhphanh.vu@gmail.com>
2021-02-27 19:33:10 -0600

    fix typo in spot.rb

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,6 @@
 class Spot < ActiveRecord::Base
   has_many :spot_students
-  hash_many :users, :through => :spot_students
+  has_many :users, :through => :spot_students
   belongs_to :user
   belongs_to :course
 end
