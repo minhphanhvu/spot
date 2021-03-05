5fb981c922871d9bdbeedcc7b0ad18e5930c8966
Jjanzen27 <63259035+Jjanzen27@users.noreply.github.com>
2021-03-05 18:21:26 -0500

    add homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 require './config/environment'
 # Pry for debug -> take it out before deployment for production
 require 'pry'
+require 'date'
 
 class ApplicationController < Sinatra::Base
+  get '/' do
+    'hello world'
+  end
+
+  get '/index/:username' do
+    user = User.find_by(username: params[:username])
+    @spots = user.spots.where('session_datetime >= ?', DateTime.now)
+    @all_spots = Spot.where('session_datetime >= ?', DateTime.now).order(session_datetime: :desc)
+    @previous_spots = user.spots.where('session_datetime <= ?', DateTime.now)
+    erb :index, layout: :layout
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :spot_students, foreign_key: 'student_id'
   has_many :spots, :through => :spot_students
-  has_many :spot_students
 end
